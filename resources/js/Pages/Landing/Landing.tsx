4edbeeeeef5f8162f66ec62e286785915d456541
Hendrie Ramdhani <50083363+ramdhanihendrie@users.noreply.github.com>
2023-12-01 05:07:28 +0700

    Hero Section

--- resources/js/Pages/Landing/Landing.tsx
+++ resources/js/Pages/Landing/Landing.tsx
@@ -1,10 +1,11 @@
 import LandingLayout from "@/Layouts/LandingLayout";
+import Hero from "./Section/Hero";
 
 export default function Landing() {
     return (
         <LandingLayout>
             <div className="container mx-auto py-3">
-                Landing
+                <Hero />
             </div>
         </LandingLayout>
     );
---
+++ resources/js/Pages/Landing/Section/Hero.tsx
@@ -0,0 +1,22 @@
+
+export default function Hero (){
+  return (
+    <section id="hero" className="flex flex-col sm:flex-row-reverse justify-center sm:justify-around py-10 text-center sm:text-left items-center">
+      <div>
+        <img src="storage/images/me.png" alt="Picture of the author"  />
+      </div>
+      <div className="mt-5 sm:mt-0">
+        <h2 className="text-3xl font-semibold">Hello, I'm</h2>
+        <h1 className="text-5xl font-semibold">Hendrie Ramdhani</h1>
+        <p className="py-6">Front-End Web Developer</p>
+        <a 
+          className="bg-indigo-500 rounded px-3 py-2 text-white" 
+          href="storage/files/Hendrie Ramdhani-resume.pdf" 
+          download
+        >
+          Download CV
+        </a>
+      </div>
+    </section>
+  );
+}
