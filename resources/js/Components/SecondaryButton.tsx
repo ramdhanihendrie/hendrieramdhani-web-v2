97046ac615f2b7247acf5816cb94a6b42dde606a
Hendrie Ramdhani <50083363+ramdhanihendrie@users.noreply.github.com>
2023-12-06 10:52:55 +0700

    Enhance Project Section

--- resources/js/Components/SecondaryButton.tsx
+++ resources/js/Components/SecondaryButton.tsx
@@ -6,7 +6,7 @@ export default function SecondaryButton({ type = 'button', className = '', disab
             {...props}
             type={type}
             className={
-                `inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 ${
+                `px-4 py-2 bg-white border-2 border-indigo-500 rounded-md font-semibold text-xs text-gray-700 tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 ${
                     disabled && 'opacity-25'
                 } ` + className
             }
--- resources/js/Pages/Landing/Partials/ProjectCard.tsx
+++ resources/js/Pages/Landing/Partials/ProjectCard.tsx
@@ -1,3 +1,6 @@
+import PrimaryButton from "@/Components/PrimaryButton";
+import SecondaryButton from "@/Components/SecondaryButton";
+
 interface ProjectCardProps {
   title: string,
   desc: string,
@@ -14,13 +17,21 @@ export default function ProjectCard ({
   image
 } : ProjectCardProps) {
   return (
-    <div className="bg-gray-700 rounded p-3 flex flex-col">
+    <div className="bg-gray-200 dark:bg-gray-700 rounded-md p-3 flex flex-col">
       <img  src={image}/>
       <h4 className="text-indigo-500 text-xl font-semibold mt-3">{title}</h4>
-      <p className="text-justify text-white">{desc}</p>
+      <p className="text-justify">{desc}</p>
       <div className="flex mt-auto justify-between gap-2 pt-2">
-        <a className="bg-indigo-500 border-indigo-500 border-2 border-solid  hover:bg-[#2a1a86] text-white font-bold py-2 px-4 rounded basis-1/2 text-center" href={link} target="_blank" rel="noreferrer">View Live</a>
-        <a className="border-indigo-500 border-2 border-solid hover:border-[#2a1a86] text-white font-bold py-2 px-4 rounded basis-1/2 text-center" href={repo} target="_blank" rel="noreferrer">Repository</a>
+        <PrimaryButton className="w-full">
+          <a href={link} target="_blank" rel="noreferrer">
+              View Live
+          </a>
+        </PrimaryButton>
+        <SecondaryButton className="w-full">
+          <a href={repo} target="_blank" rel="noreferrer">
+              Repository
+          </a>
+        </SecondaryButton>
       </div>
     </div>
   );
